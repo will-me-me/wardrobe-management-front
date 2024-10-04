ebb4187d31a8c3960699e326b8774dca658acb4f
“willy <[email protected]>
2024-10-04 11:17:40 +0300

    added more password validation rules

--- src/components/CreateUser.vue
+++ src/components/CreateUser.vue
@@ -36,11 +36,29 @@
             @click:append="showPassword = !showPassword"
             hint="At least 8 characters"
           />
+
+          <v-list>
+            <v-list-item
+              v-for="(rule, index) in passwordValidationRules"
+              :key="index"
+            >
+              <v-icon :color="rule.valid ? 'green' : 'red'">{{
+                rule.valid ? "mdi-check-circle" : "mdi-close-circle"
+              }}</v-icon>
+              <v-list-item-content>
+                <v-list-item-title>{{ rule.message }}</v-list-item-title>
+              </v-list-item-content>
+            </v-list-item>
+          </v-list>
         </v-form>
       </v-card-text>
       <v-card-actions>
         <v-spacer />
-        <v-btn color="green darken-1" text @click="createNewUser()"
+        <v-btn
+          :disabled="!valid || !isPasswordValid"
+          color="green darken-1"
+          text
+          @click="createNewUser()"
           >Create User</v-btn
         >
         <v-btn color="grey" text @click="$emit('update:dialog', false)"
@@ -53,7 +71,7 @@
 
 <script>
 import { useRouter } from "vue-router";
-import { ref } from "vue";
+import { ref, watch, computed } from "vue";
 
 import { ClothesStore } from "../../src/stores/wordRobe";
 
@@ -81,6 +99,52 @@ export default {
 
     const showPassword = ref(false);
 
+    const passwordValidationRules = ref([
+      {
+        message: "At least 8 characters",
+        valid: false,
+      },
+      {
+        message: "At least one uppercase letter",
+        valid: false,
+      },
+      {
+        message: "At least one lowercase letter",
+        valid: false,
+      },
+      {
+        message: "At least one number",
+        valid: false,
+      },
+      {
+        message: "At least one special character",
+        valid: false,
+      },
+    ]);
+
+    // Watch password input and update the validity of the rules dynamically
+    watch(
+      () => user.value.password,
+      (newPassword) => {
+        passwordValidationRules.value = [
+          { message: "At least 8 characters", valid: newPassword.length >= 8 },
+          {
+            message: "At least one uppercase letter",
+            valid: /[A-Z]/.test(newPassword),
+          },
+          {
+            message: "At least one lowercase letter",
+            valid: /[a-z]/.test(newPassword),
+          },
+          { message: "At least one number", valid: /\d/.test(newPassword) },
+          {
+            message: "At least one special character",
+            valid: /[!@#$%^&*]/.test(newPassword),
+          },
+        ];
+      }
+    );
+
     // Validation rules
     const rules = {
       required: (v) => !!v || "This field is required",
@@ -105,6 +169,10 @@ export default {
       }
     };
 
+    const isPasswordValid = computed(() => {
+      return passwordValidationRules.value.every((rule) => rule.valid);
+    });
+
     // Form validation
     const validateForm = () => {
       return new Promise((resolve) => {
@@ -121,10 +189,12 @@ export default {
     return {
       valid,
       user,
+      showPassword,
+      passwordValidationRules,
       createNewUser,
       closeDialog,
       rules,
-      showPassword,
+      isPasswordValid,
     };
   },
 };
